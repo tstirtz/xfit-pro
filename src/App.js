9a68fe72e4facfdf2d14de629d703f94aafb3067
Michael DeHaan <mdehaan51@gmail.com>
2018-06-04 12:21:17 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Header from './user-page/header/header';
-<<<<<<< HEAD
+
 import SideBar from './user-page/sidebar/sidebar';
-=======
+
 import StatusFeed from './user-page/status-feed/status-feed';
 
 
