bbbed953b12c488eac862df7e861440768c8b6cc
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-06-04 19:44:21 -0700

    Add rule for jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,27 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      "prefer-promise-reject-errors": ["error", {"allowEmptyReject": true}],
-      "func-names": ["error", "never"],
-      // "promise/catch-or-return": "error",
-    }
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+},
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "ecmaFeatures": {
+        "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "prefer-promise-reject-errors": ["error", {"allowEmptyReject": true}],
+    "func-names": ["error", "never"],
+    "no-const-assign": "warn",
+    "no-this-before-super": "warn",
+    "no-undef": "warn",
+    "no-unreachable": "warn",
+    "no-unused-vars": "warn",
+    "constructor-super": "warn",
+    "valid-typeof": "warn"
+  }
 };
