bf9588d38577234ed61281938c50da5b65ea695e
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-06-04 19:45:14 -0700

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from './user-page/header/header';
-
 import SideBar from './user-page/sidebar/sidebar';
-
 import StatusFeed from './user-page/status-feed/status-feed';
+import About from './about-page/about';
 
-
-class App extends Component {
-  render() {
-    return (
+// TODO
+// Set up pictures route and gym route
+function App() {
+  return (
+    <Router>
       <div className="App">
         <header className="App-header">
           <Header />
         </header>
-
-      	<div className = "sidebar">
-      		<SideBar />
-      	</div>
+        <main>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/pictures"/>
+          <Route exact path="/gym"/>
+          {/* <Route exact path="/" component={}/> */}
+        </main>
+        <div className = "sidebar">
+          <SideBar />
+        </div>
 
         <StatusFeed />
 
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
